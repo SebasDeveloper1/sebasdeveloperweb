e28cfb43cbfed45fefd2915a546e7832f8af49bb
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2024-04-24 17:09:34 -0500

    :lipstick: Add intersection observer project page

--- app/ui/components/projects/ProjectCard/ProjectCard.tsx
+++ app/ui/components/projects/ProjectCard/ProjectCard.tsx
@@ -1,26 +1,36 @@
+'use client';
 import Image from 'next/image';
 import { ProjectCardProps } from './ProjectCard.model';
 import Link from 'next/link';
+import { useInView } from 'react-intersection-observer';
 
 export function ProjectCard({ projectData }: ProjectCardProps) {
   const { name, slug, level, imagesCollection } = projectData;
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
   return (
     <Link
+      ref={ref}
       href={`/projects/${slug}`}
       className="group relative h-full overflow-hidden w-full aspect-[4/3] mb-4 md:mb-6 rounded-xl"
     >
-      <figure className="relative w-full h-full">
-        <Image
-          fill
-          sizes="100%"
-          src={`${imagesCollection?.items[0]?.url}`}
-          alt={`${imagesCollection?.items[0]?.title}`}
-          placeholder="blur"
-          loading="lazy"
-          className="object-cover object-center lg:transform lg:group-hover:scale-150 lg:transition-all"
-          blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
-        />
-      </figure>
+      {inView ? (
+        <figure className="relative w-full h-full">
+          <Image
+            fill
+            sizes="100%"
+            src={`${imagesCollection?.items[0]?.url}`}
+            alt={`${imagesCollection?.items[0]?.title}`}
+            placeholder="blur"
+            loading="lazy"
+            className={`object-cover object-center lg:transform lg:group-hover:scale-150 lg:transition-all ${!inView ? 'opacity-0' : 'animate-fade-in duration-300 opacity-100'}`}
+            blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+          />
+        </figure>
+      ) : null}
 
       <section className="absolute inset-0 bg-gradient-to-t from-gray-950 via-transparent to-transparent">
         <article className="flex flex-col justify-end w-full h-full p-2 md:p-3">
