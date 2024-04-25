282906a05d1dd65c09e56d2a3d02e5fb8b280eb7
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2024-04-25 18:22:07 -0500

    :sparkles: Add infinity scrolling in project list

--- app/ui/components/projects/ProjectList/ProjectList.model.ts
+++ app/ui/components/projects/ProjectList/ProjectList.model.ts
@@ -3,3 +3,9 @@ import { ProjectsCollection } from '@/app/lib/api/generated/graphql';
 export interface ProjectListProps {
   projectsCollection: ProjectsCollection;
 }
+
+export interface NavbarItems {
+  name: string;
+  title: string;
+  icon: JSX.Element;
+}
--- app/ui/components/projects/ProjectList/ProjectList.tsx
+++ app/ui/components/projects/ProjectList/ProjectList.tsx
@@ -1,27 +1,81 @@
 'use client';
-import { useState } from 'react';
-import { ProjectListProps } from './ProjectList.model';
+import { useEffect, useState } from 'react';
+import { NavbarItems, ProjectListProps } from './ProjectList.model';
 import { Projects } from '@/app/lib/api/generated/graphql';
 import { ProjectCard } from '../ProjectCard';
 import { navbarItems } from './NavbarItems';
+
+const initialCount = 4;
 
 export function ProjectList({ projectsCollection }: ProjectListProps) {
   const { items } = projectsCollection;
   const [filteredProjects, setFilteredProjects] = useState<Projects[]>(
     items as Projects[],
   );
-  const [activeItemId, setActiveItemId] = useState<string>('all');
+  const [activeItem, setActiveItem] = useState<NavbarItems>(navbarItems[0]);
+  const [visibleItems, setVisibleItems] = useState<number>(initialCount); // Número de elementos a mostrar inicialmente
 
-  const handleMenuClick = ({ itemId }: { itemId: string }) => {
-    const updatedProjects =
-      itemId === 'all'
+  /**
+   * Handle scroll event to load more items when reaching the bottom of the page.
+   */
+  useEffect(() => {
+    const handleScroll = (): void => {
+      const isScrolledToBottom =
+        document.documentElement.offsetHeight -
+          (window.innerHeight + document.documentElement.scrollTop) <=
+        200;
+
+      if (!isScrolledToBottom) return;
+
+      // Calculate the new number of visible items
+      const newVisibleItems = Math.min(
+        filteredProjects.length,
+        visibleItems + initialCount,
+      );
+
+      // Update the visible items count
+      setVisibleItems(newVisibleItems);
+    };
+
+    // Add scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup: remove scroll event listener
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [filteredProjects, visibleItems]);
+
+  /**
+   * Handle menu item click event.
+   *
+   * Updates the visible items based on the selected menu item.
+   *
+   * @param {NavbarItems} item - The selected menu item.
+   */
+  const handleMenuClick = ({ item }: { item: NavbarItems }): void => {
+    // Filter certifications based on the selected menu item
+    const updatedCertification: Projects[] =
+      item.name === 'all'
         ? (items as Projects[])
         : (items.filter(
-            (project) => project?.levelkey === itemId,
+            (project) => project?.category === item?.name,
           ) as Projects[]);
 
-    setFilteredProjects(updatedProjects);
-    setActiveItemId(itemId);
+    // Determine the number of visible items
+    const visibleItemCount = Math.min(
+      updatedCertification.length,
+      initialCount,
+    );
+
+    // Update the visible items count
+    setVisibleItems(visibleItemCount);
+
+    // Set the filtered certifications
+    setFilteredProjects(updatedCertification);
+
+    // Set the active menu item
+    setActiveItem(item);
   };
 
   return (
@@ -34,8 +88,8 @@ export function ProjectList({ projectsCollection }: ProjectListProps) {
                 <li key={`item-${item?.name}`}>
                   <button
                     type="button"
-                    className={`group w-full flex flex-col items-center gap-4 span-sm font-semibold capitalize whitespace-nowrap text-light-500 hover:text-light-700 dark:text-dark-400 hover:dark:text-dark-200 ${activeItemId === item?.name ? 'text-primary-500 dark:text-primary-500' : ''}`}
-                    onClick={() => handleMenuClick({ itemId: item?.name })}
+                    className={`group w-full flex flex-col items-center gap-4 span-sm font-semibold capitalize whitespace-nowrap text-light-500 hover:text-light-700 dark:text-dark-400 hover:dark:text-dark-200 ${activeItem?.name === item?.name ? 'text-primary-500 dark:text-primary-500' : ''}`}
+                    onClick={() => handleMenuClick({ item })}
                   >
                     {item?.icon}
                     {item?.title}
@@ -45,10 +99,10 @@ export function ProjectList({ projectsCollection }: ProjectListProps) {
             </ul>
           </header>
           <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 gap-4 md:gap-6 w-11/12 lg:w-10/12">
-            {filteredProjects.map((project) => {
+            {filteredProjects.slice(0, visibleItems).map((project) => {
               return (
                 <li
-                  key={`project_${project?.sys?.id}`}
+                  key={`project_${project?.name}`}
                   className={`overflow-hidden w-full aspect-[4/3] rounded-xl`}
                 >
                   <ProjectCard projectData={project as Projects} />
