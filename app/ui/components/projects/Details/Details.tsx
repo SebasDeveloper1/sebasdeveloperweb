f9fe3879ae2afa35e05fba6c90adfcde5ece3754
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2024-04-30 20:28:46 -0500

    :lipstick: Add animation Project details

--- app/ui/components/projects/Details/Details.tsx
+++ app/ui/components/projects/Details/Details.tsx
@@ -16,12 +16,12 @@ export async function Details({ slug }: DetailsProps): Promise<JSX.Element> {
   const projectData = projectsCollection?.items[0];
   const projectImages = projectData?.imagesCollection?.items;
   return (
-    <section className="w-full h-full min-height-banner py-16 bg-light-50 dark:bg-dark-950">
+    <section className="overflow-hidden w-full h-full min-height-banner py-16 bg-light-50 dark:bg-dark-950">
       <section className="grid grid-cols-1 lg:grid-cols-2 justify-items-center gap-x-14 gap-y-6 w-full h-full min-height-banner lg:pt-12">
-        <article className="order-2 lg:order-1 col-span-1 lg:col-span-1 flex justify-end w-11/12 lg:w-full">
+        <article className="order-2 lg:order-1 col-span-1 lg:col-span-1 flex justify-end w-11/12 lg:w-full animate-fade-in-down">
           <Description projectData={projectData as Projects} />
         </article>
-        <article className="order-1 lg:order-2 col-span-1 lg:col-span-1 w-full">
+        <article className="order-1 lg:order-2 col-span-1 lg:col-span-1 w-full animate-fade-in-up">
           <ProjectSlider projectImages={projectImages as Asset[]} />
         </article>
       </section>
