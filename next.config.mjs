34c8bde057f23e62a4cd54eb518aa83e2bef214b
SebasDeveloper1 <sebaspedroza.developer@gmail.com>
2026-08-05 20:39:15 -0500

    🐛 Fix readme stats origin

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'github-readme-stats.vercel.app' },
+      { protocol: 'https', hostname: 'github-readme-stats-eta-livid-88.vercel.app' },
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
       { protocol: 'https', hostname: 'static.platzi.com' },
       { protocol: 'https', hostname: 'images.ctfassets.net' },
